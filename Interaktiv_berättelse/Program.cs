906e868fd8781b352c5ef7ad7ef3b62880c1db0f
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2023-09-04 09:58:42 +0200

    First if: yes or no

--- Interaktiv_berättelse/Program.cs
+++ Interaktiv_berättelse/Program.cs
@@ -4,13 +4,42 @@ String name = Console.ReadLine();
 Console.WriteLine($"Welcome to the game {name}!");
 Thread.Sleep(1000);
 // Start info
-Console.WriteLine("Answer a, b or c to take different paths in the game.");
+Console.WriteLine("Type in a, b or c to make your choices in the game.");
 Thread.Sleep(1000);
 Console.WriteLine("Keep in mind that there are X possible endings..."); 
 Thread.Sleep(1000);
 Console.WriteLine("...and perhaps a few secret ones as well");
+Thread.Sleep(1500);
 // game start :)
 Console.WriteLine("Well then, are you ready to start the adventure?");
-Thread.Sleep(500);
+Thread.Sleep(600);
 Console.WriteLine("Write yes or no");
-String start = Console.ReadLine();
+String start = Console.ReadLine().ToLower();
+
+if (start == "no")
+{
+Thread.Sleep(1000);
+Console.Write(".");
+Thread.Sleep(700);
+Console.Write(".");
+Thread.Sleep(700);
+Console.Write(".");
+Thread.Sleep(1500);
+Console.Write("oh.");
+Thread.Sleep(1500);
+Console.WriteLine("Well");
+Thread.Sleep(1000);
+Console.WriteLine("Game over then, I guess??");
+}
+else if (start == "yes")
+{
+Console.WriteLine("Let's begin!");
+Thread.Sleep(1000);
+}
+
+
+Console.ReadLine();
+// if (start != "no" || start == "NO")
+// {
+//     Console.WriteLine("ghasdujiflknrhPJDSL");
+// }
